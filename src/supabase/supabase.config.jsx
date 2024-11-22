1b4342994d2a788fc0563c6a374bbad96bbcf17f
Eduardo Trevino <seduardolothric@gmail.com>
2024-11-21 23:55:23 -0600

    Cambio en supabase

--- src/supabase/supabase.config.jsx
+++ src/supabase/supabase.config.jsx
@@ -1,10 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-// Asegúrate de que estas variables de entorno estén definidas y sean accesibles
 const supabaseUrl = import.meta.env.VITE_APP_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_APP_SUPABASE_ANON_KEY;
 
-// Crea un único cliente de Supabase para interactuar con tu base de datos
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 // Opcional: Agrega manejo de errores para la inicialización
